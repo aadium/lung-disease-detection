9bd54805d3a66375335418b249f930961e05da2e
Aadi Umrani <aadi.p.um@gmail.com>
2024-06-06 20:23:09 -0400

    chore: Using Python instead of a Jupyter Notebook as it bloats up the language bar

---
+++ model/train/train.py
@@ -0,0 +1,143 @@
+import os
+import cv2
+import numpy as np
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import os
+from sklearn.model_selection import train_test_split
+import cv2
+from sklearn.utils import shuffle
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras.applications import ResNet50 # type: ignore
+from tensorflow.keras.layers import Dense, Flatten, GlobalAveragePooling2D # type: ignore
+from tensorflow.keras.models import Model # type: ignore
+from tensorflow.keras.callbacks import EarlyStopping # type: ignore
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+X_train = []
+Y_train = []
+image_size = 150
+labels = ['Bacterial Pneumonia','Corona Virus Disease', 'Normal', 'Tuberculosis', 'Viral Pneumonia']
+
+for label in labels:
+    folderPath = os.path.join('dataset/train', label)
+    for filename in os.listdir(folderPath):
+        img = cv2.imread(os.path.join(folderPath, filename))
+        img = cv2.resize(img, (image_size, image_size))
+        X_train.append(img)
+        Y_train.append(label)
+
+for label in labels:
+    folderPath = os.path.join('dataset/test', label)
+    for filename in os.listdir(folderPath):
+        img = cv2.imread(os.path.join(folderPath, filename))
+        img = cv2.resize(img, (image_size, image_size))
+        X_train.append(img)
+        Y_train.append(label)
+        
+for label in labels:
+    folderPath = os.path.join('dataset/val', label)
+    for filename in os.listdir(folderPath):
+        img = cv2.imread(os.path.join(folderPath, filename))
+        img = cv2.resize(img, (image_size, image_size))
+        X_train.append(img)
+        Y_train.append(label)
+
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)
+
+X_train, Y_train = shuffle(X_train, Y_train, random_state=100)
+
+x_train, x_test, y_train, y_test = train_test_split(X_train, Y_train, test_size=0.2, random_state=100)
+
+y_train = tf.keras.utils.to_categorical([labels.index(label) for label in y_train], num_classes=len(labels))
+y_test = tf.keras.utils.to_categorical([labels.index(label) for label in y_test], num_classes=len(labels))
+
+print('Training data shape: ', x_train.shape)
+print('Training labels shape: ', y_train.shape)
+print('Testing data shape: ', x_test.shape)
+print('Testing labels shape: ', y_test.shape)
+print('Number of classes: ', len(labels))
+
+base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(image_size, image_size, 3))
+
+# Freeze the pre-trained layers
+for layer in base_model.layers:
+    layer.trainable = False
+
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(256, activation='relu')(x)
+x = Dense(256, activation='relu')(x)
+output = Dense(5, activation='softmax')(x)
+
+# Create the final model
+model = Model(inputs=base_model.input, outputs=output)
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.summary()
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+
+try:
+    model.optimizer
+except AttributeError:
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+history = model.fit(
+    x_train, y_train, epochs=50, validation_split=0.1,
+    callbacks=[early_stopping]
+)
+
+model.save('model/lung_disease_detection_model.h5')
+
+y_pred = model.predict(x_test)
+y_pred_classes = np.argmax(y_pred, axis=1)
+y_true = np.argmax(y_test, axis=1)
+
+accuracy = accuracy_score(y_true, y_pred_classes)
+f1 = f1_score(y_true, y_pred_classes, average='weighted')
+precision = precision_score(y_true, y_pred_classes, average='weighted')
+recall = recall_score(y_true, y_pred_classes, average='weighted')
+
+print(f'Accuracy: {accuracy}')
+print(f'F1 Score: {f1}')
+print(f'Precision: {precision}')
+print(f'Recall: {recall}')
+
+class_names = ['Bacterial Pneumonia','Corona Virus Disease', 'Normal', 'Tuberculosis', 'Viral Pneumonia']
+
+cm = confusion_matrix(y_true, y_pred_classes)
+
+plt.figure(figsize=(10, 8))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=class_names, yticklabels=class_names)
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Confusion Matrix')
+plt.show()
+
+accuracy = history.history['accuracy']
+val_accuracy  = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(1, 2, figsize=(20, 3))
+ax = ax.ravel()
+
+for i, met in enumerate(['accuracy', 'loss']):
+    ax[i].plot(history.history[met])
+    ax[i].plot(history.history['val_' + met])
+    ax[i].set_title('Model {}'.format(met))
+    ax[i].set_xlabel('epochs')
+    ax[i].set_ylabel(met)
+    ax[i].legend(['train', 'val'])
+
+plt.show()
